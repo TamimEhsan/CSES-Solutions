3ca7d9a002b8d14269642018302de9ff4c953c64
Tamim Ehsan <tamimehsan99@gmail.com>
2025-06-07 20:46:09 +0600

    remove unused files

--- Additional Problems II/Mex Grid Queries.cpp
+++ Additional Problems II/Mex Grid Queries.cpp
@@ -1,51 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define INF 2047483647
-#define INFL 9223372036854775807
-#define int long long
-#define ll long long
-#define pii pair<ll,ll>
-#define F first
-#define S second
-#define mp make_pair
-#define pb push_back
-#define ull unsigned long long
-//#define M 1000000007
-#define M 998244353
-#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
-#define take(x) scanf("%d",&x)
-#define DE(x) printf("\ndebug %d\n",x);
-#define vout(x) for(int i=0;i<x.size();i++) printf("%d ",x[i]);
-#define pie acos(-1)
-#define MOD 998244353
-#define endl '\n'
- 
-int Set(int N,int pos){return N=N | (1<<pos);}
-int reset(int N,int pos){return N= N & ~(1<<pos);}
-bool check(int N,int pos){return (bool)(N & (1<<pos));}
- 
-#define int long long
- 
-int n;
-int recurse(int m){
-    if(m == 0) return n;
- 
-}
- 
-void solve(){
-    int m;
+
+int main(){
+    int n,m;
     cin>>n>>m;
     cout<<((n-1)^(m-1));
- 
- 
- 
 }
- 
-signed main(){
-    FASTIO;
-    int tc=1;
-    //cin>>tc;
- 
-    while(tc--) solve();
-}
- 
--- Additional Problems/Number Grid.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int n,m;
-    cin>>n>>m;
-    cout<<((n-1)^(m-1));
-}
